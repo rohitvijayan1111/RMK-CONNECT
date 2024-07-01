647151826bdeb23f63aee60d49e984e30667382a
likesh50 <like22050.it@rmkec.ac.in>
2024-07-01 15:44:38 +0530

    Login Page final

--- FrontEnd/RMKEC/src/Components/Login.jsx
+++ FrontEnd/RMKEC/src/Components/Login.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Login.css';
+import logo from '../assets/Logo.png';
 
-export const Login = () => {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="login-form">
+      
+        <div className="flower-logo"><img src={logo}/> </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            id="username"
+            placeholder='USERNAME'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            id="password"
+            placeholder='PASSWORD'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Sign in</button>
+      </form>
+      
+    </div>
+  );
 }
+
+export default Login;
