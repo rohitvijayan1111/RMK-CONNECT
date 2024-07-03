c3579e6579a7cabdef5b454efc48a566d860a77e
likesh50 <like22050.it@rmkec.ac.in>
2024-07-03 10:39:06 +0530

    Navigation bar

---
+++ FrontEnd/RMKEC/src/Components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navigation.css'
+function Navigation() {
+  return (
+    <div className='navigation'>
+        <p className='head'>DashBoard</p>
+        <p>Home - Dashboard </p>
+    </div>
+  )
+}
+
+export default Navigation
--- FrontEnd/RMKEC/src/Pages/DashBoard.jsx
+++ FrontEnd/RMKEC/src/Pages/DashBoard.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './DashBoard.css'
 import NavBar from '../Components/NavBar'
 import SideBar from '../Components/SideBar'
+import Navigation from '../Components/Navigation'
 
 function DashBoard() {
   return (
     <div className='dashboard'>
         <NavBar/>
-        <SideBar/>
+        <div className="content">
+          <SideBar/>
+          <Navigation/>
+        </div>
+        
    
     </div>
   )
